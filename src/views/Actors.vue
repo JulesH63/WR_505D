07a60d17f7a5f5202087ce0dbc6877a19dca0aaf
Jules H <jules.hemon@etudiant.univ-reims.fr>
2024-02-27 11:18:16 +0100

    feat/ movie card

--- src/views/Actors.vue
+++ src/views/Actors.vue
@@ -1,8 +1,38 @@
 <script setup>
+import { onMounted, ref, toRaw } from "vue";
+import axios from "axios";
+import ActorCard from "../components/ActorCard.vue";
+
+let data = ref("");
+let token = localStorage.getItem("token");
+
+onMounted(async () => {
+  const response = await axios.get("http://localhost/api/actors?page=1", {
+    headers: {
+      Accept: "application/ld+json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  data.value = response.data["hydra:member"];
+  console.log(toRaw(data.value));
+});
 </script>
 
 <template>
-  <main>
-    <h1>Actors</h1>
-  </main>
+  <div class="gallery">
+    <ActorCard v-for="actor in data" :key="actor.id" :actor="actor" />
+  </div>
 </template>
+
+<style scoped lang="scss">
+.gallery {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 2em auto;
+  gap: 2em;
+  padding: 2em;
+  max-width: 1200px; 
+  border-radius: 12px;
+}
+</style>
--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,8 +1,50 @@
+<template>
+  <div class="movie-list">
+    <div class="title">
+      <h1>FILMS</h1>
+    </div>
+    <div class="gallery">
+      <MovieCard v-for="movie in data" :key="movie.id" :movie="movie" />
+    </div>
+  </div>
+</template>
+
 <script setup>
+import { onMounted, ref } from "vue";
+import axios from "axios";
+import MovieCard from "../components/MovieCard.vue";
+
+let data = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get(
+      "http://localhost/laragon/www/WR_505D/public/index.php/api/movies?page=1",
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+    data.value = response.data;
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+  }
+});
 </script>
 
-<template>
-  <main>
-    <h1>Movies</h1>
-  </main>
-</template>
+<style scoped lang="scss">
+.movie-list {
+  .title {
+    text-align: center;
+    margin-bottom: 1em;
+  }
+
+  .gallery {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    gap: 1em;
+    justify-items: center;
+  }
+}
+</style>
